9313f7a7ddbe1b7d2066eb1728e8b05dc65a3cfc
Rajnish <errajnishkumarme@gmail.com>
2022-12-26 12:54:09 +0530

    updated class

--- StudentManagement/src/main/java/com/student/management/entity/Student.java
+++ StudentManagement/src/main/java/com/student/management/entity/Student.java
@@ -28,5 +28,29 @@ public class Student {
 	private String lastname;
 	@Column(name="email_Id")
 	private String email;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
